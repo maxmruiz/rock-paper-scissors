9fa6100f267c69d63bf7daf15c8f366c0bb4b3e9
Maria Martinez <mariamartinez@Marias-MacBook-Pro.local>
2024-06-28 10:26:50 -0400

    base of program

--- rps.py
+++ rps.py
@@ -6,14 +6,28 @@ player = False
 
 computer = options[randint(0, 2)]
 
-if player == False:
+while player == False:
     player = input('rock paper or scissors?\n')
     if player == computer:
         print('tie')
     elif player == 'rock':
         if computer == 'paper':
-            print('computer wins')
+            print('computer wins', computer, 'beats', player)
+        else:
+            print('player wins', player, 'beats', computer)
     elif player == 'scissors':
         if computer == 'rock':
-            print('computer wins')
+            print('computer wins', computer, 'beats', player)
+        else:
+            print('player wins', player, 'beats', computer)
+    elif player == 'paper':
+        if computer == 'scissors':
+            print('computer wins', computer, 'beats', player)
+        else:
+            print('player wins', player, 'beats', computer)
+    else:
+        print('invalid')
+    
+    player = False
+    computer = options[randint(0,2)]
     
